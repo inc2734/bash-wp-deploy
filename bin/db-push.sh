bcab4c8aac4225bfd85ccb8f60c0889b352a02c7
Takashi Kitajima <inc@2inc.org>
2017-01-22 02:43:33 +0900

    Some fixes

--- bin/db-push.sh
+++ bin/db-push.sh
@@ -12,12 +12,12 @@ CONFIG_PATH=${ROOT}/config.json
 cd ${LOCAL_SERVER_PATH}
 
 echo "===== Exporting local database =========="
+wp db export local.sql
 wp search-replace ${LOCAL_SERVER_URL} ${SERVER_URL} --url=${LOCAL_SERVER_URL} --network > /dev/null
 wp search-replace ${LOCAL_SERVER_NAME} ${SERVER_NAME} --url=${LOCAL_SERVER_URL} --network > /dev/null
-wp db export local.sql
-echo $(ls -la local.sql)
-wp search-replace ${SERVER_URL} ${LOCAL_SERVER_URL} --url=${SERVER_NAME} --network > /dev/null
-wp search-replace ${SERVER_NAME} ${LOCAL_SERVER_NAME} --url=${SERVER_NAME} --network > /dev/null
+wp db export for-remote.sql
+echo $(ls -la for-remote.sql)
+wp db import local.sql
 
 echo "===== Importing to remote database from local database =========="
-ssh ${SSH_USER}@${SSH_HOST} -p ${SSH_PORT} "mysql --host=${DB_HOST} --user=${DB_USER} --password=\"${DB_PASSWORD}\" --default-character-set=utf8 ${DB_NAME}" < local.sql
+ssh ${SSH_USER}@${SSH_HOST} -p ${SSH_PORT} "mysql --host=${DB_HOST} --user=${DB_USER} --password=\"${DB_PASSWORD}\" --default-character-set=utf8 ${DB_NAME}" < for-remote.sql
